b5f10ef3f7b11feaa0a7d33c726d8c2ec91992fa
danieljovenir <danemengao15@gmail.com>
2024-03-14 19:11:00 -0300

    teste

---
+++ gerador_pdf.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use Mpdf\Mpdf;
+
+// Cria uma instância do objeto Mpdf
+$mpdf = new Mpdf();
+
+// Adiciona conteúdo ao PDF
+$mpdf->WriteHTML('<h1>Meu primeiro PDF</h1>');
+
+// Salva o PDF no servidor ou envia para o navegador
+$mpdf->Output('meu_pdf.pdf', 'D');
